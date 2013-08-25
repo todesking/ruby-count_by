e9123ef982f699d93290c75a62e08771afc7cbf4
Julio Olivera <julio.olvr@gmail.com>
2013-08-25 12:36:52 -0300

    Added support for calling count_by without a block

--- lib/count_by.rb
+++ lib/count_by.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def count_by(&block)
-    raise ArgumentError, "count_by: block not specified" unless block_given?
+    return to_enum(:count_by) unless block_given?
 
     each_with_object(Hash.new(0)) do|elm, h|
       h[yield(elm)] += 1
